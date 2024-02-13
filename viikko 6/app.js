424294044c93e6dcc595165e4ecb3d66c349f67d
Jamheik <81734621+Jamheik@users.noreply.github.com>
2024-02-13 17:14:06 +0200

    teht 15: app.js tehty get ja post, kuvat omassa kansiossa

--- viikko 6/app.js
+++ viikko 6/app.js
@@ -16,5 +16,39 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 module.exports = app;
+
+app.get('/esim',
+    function(request,response){
+        response.send('esimerkki');
+        console.log('esimerkki');
+    }
+);
+
+app.get('/nimi/:nimi',
+    function(request,response){
+        response.send('nimeni on ' +  request.params.nimi);
+        console.log('esimerkki');
+    }
+);
+
+app.get('/nimi/:nimi/:sukunimi',
+    function(request,response){
+        response.send('nimeni on ' +  request.params.nimi + " " + request.params.sukunimi);
+        console.log('esimerkki');
+    }
+);
+
+app.use(
+    function(req,res,next){
+        console.log('The common middleware called');
+        next();
+    }
+);
+
+app.post('/',
+    function(request,response){
+        response.send(request.body.fname + ' '+  request.body.lname);
+        console.log(request.body.fname + ' '+  request.body.lname);
+    }
+);
